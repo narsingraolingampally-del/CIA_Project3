f7f868120d21e3357018010d3a7a585f4cd3d6a1
Narsing <narsingrao.lingampally@gmail.com>
2026-09-01 12:47:48 +0530

    Fix SQL Server ODBC driver installation on Render

--- render-build.sh
+++ render-build.sh
@@ -1,28 +1,38 @@
+```bash
 #!/usr/bin/env bash
-set -o errexit
+set -e
+
+echo "========================================"
+echo "Installing system dependencies"
+echo "========================================"
+
+apt-get update
+
+apt-get install -y \
+    curl \
+    gnupg \
+    unixodbc \
+    unixodbc-dev
 
 echo "========================================"
 echo "Installing Microsoft ODBC Driver 18"
 echo "========================================"
 
-# Install Microsoft repository package
-curl -sSL -O https://packages.microsoft.com/config/debian/$(grep VERSION_ID /etc/os-release | cut -d '"' -f 2 | cut -d '.' -f 1)/packages-microsoft-prod.deb
+# Add Microsoft package repository
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc \
+    | gpg --dearmor \
+    > /usr/share/keyrings/microsoft-prod.gpg
 
-dpkg -i packages-microsoft-prod.deb
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-prod.gpg] https://packages.microsoft.com/debian/12/prod bookworm main" \
+    > /etc/apt/sources.list.d/mssql-release.list
 
-rm packages-microsoft-prod.deb
-
-# Update package lists
 apt-get update
 
 # Install Microsoft ODBC Driver 18
 ACCEPT_EULA=Y apt-get install -y msodbcsql18
 
-# Install unixODBC development libraries
-apt-get install -y unixodbc-dev
-
 echo "========================================"
-echo "Checking installed ODBC drivers"
+echo "Checking ODBC drivers"
 echo "========================================"
 
 odbcinst -q -d
@@ -31,6 +41,7 @@ echo "========================================"
 echo "Installing Python dependencies"
 echo "========================================"
 
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "========================================"
@@ -48,3 +59,4 @@ python manage.py migrate
 echo "========================================"
 echo "BUILD COMPLETE"
 echo "========================================"
+```
